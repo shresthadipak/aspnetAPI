96c34058cd65c1a46183beb9e78dcd6e64b2489f
Dipak Shrestha <shresthadipak2049@gmail.com>
2023-08-10 15:15:39 +0545

    Add project files.

---
+++ aspnetAPI/Controllers/TestController.cs
@@ -0,0 +1,57 @@
+﻿using aspnetAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private static List<Test> tests = new List<Test>
+        {
+            new Test
+            {
+                Id=1,
+                Name="SpiderMan",
+                Place = "New York"
+            },
+            new Test
+            {
+                Id=2,
+                Name="Iron Man",
+                Place = "Virginia"
+            },
+            new Test
+            {
+                Id=3,
+                Name="Superman",
+                Place = "Texas"
+            },
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Test>>> GetAllTest()
+        {
+            return Ok(tests);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Test>>> GetSingleTest(int id)
+        {
+            var test = tests.Find(y => y.Id == id);
+            if(test is null)
+            {
+                return NotFound("Sorry!!, " +id+ " is not found.");
+            }
+            return Ok(test);
+        }
+
+        [HttpPost("addTest")]
+        public async Task<ActionResult<List<Test>>> AddTest([FromBody] Test test)
+        {
+            tests.Add(test);
+            return Ok(tests);
+        }
+    }
+}
---
+++ aspnetAPI/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿namespace aspnetAPI.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
